1212fc61c538467a6311470d6bbb49001d75583c
thiagoassisk8 <tyagosilva82@gmail.com>
2024-04-21 19:08:47 -0300

    first commit initializingProject

---
+++ src/main/java/com/qikserver/grocery/SpringBootGroceryApplication.java
@@ -0,0 +1,13 @@
+package com.qikserver.grocery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGroceryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGroceryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qikserver/grocery/SpringBootGroceryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qikserver.grocery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGroceryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
